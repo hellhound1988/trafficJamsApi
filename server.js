d95a54d184f3b5fde0ac731cbdf7df54f60367bb
Mikhail Beschastnov <mbeschastnov@news360.ru>
2016-11-30 21:31:08 -0800

    node.js implementation

--- server.js
+++ server.js
@@ -1,67 +1,113 @@
 #!/usr/bin/env node
-var nodePersist = require('node-persist');
 var express = require('express');
-var path = require('path');
 var fs = require('fs');
 var http = require('http');
 var jsdom = require("jsdom");
 var app = express();
 
+/**
+ * Инициализация сервера Express для обработки http-запросов
+ */
+
+// статический контент
 app.use(express.static(__dirname + "/"));
 
-app.post("^/routes$", function (req, res) {
-    res.writeHead(200);
-    executeRequest(res);
-});
+/**
+ * @typedef {object} RouteInfo
+ * @property {string} data - входные данные
+ * @property {string} route - результат
+ */
 
-nodePersist.initSync();
-
-var html = '<html><head><script src="http://api-maps.yandex.ru/2.0/?load=package.standard,package.geoObjects,package.route&lang=ru-RU" type="text/javascript"></script></head><body></body></html>';
-var doc = jsdom.jsdom(html, {
-    features: {
-        FetchExternalResources   : ['script'],
-        ProcessExternalResources : ['script'],
-        MutationEvents           : '2.0'
+/**
+ * GET /route?data=<data> - запрос маршрута с параметром
+ * @return {RouteInfo}
+ */
+app.get("^/route$", function (req, res) {
+    var line = req.query.data;
+    if (line) {
+        res.setHeader("Content-Type", "application/json");
+        res.writeHead(200);
+        calculateRoute(line).then(function (route) {
+            res.write(JSON.stringify(route));
+            res.end();
+        });
+    } else {
+        res.writeHead(400);
+        res.write("'data' parameter is missing");
+        res.end();
     }
 });
 
-var window = doc.defaultView;
-window.onload = function() {
-    window.ymaps.ready(function() {
-        // configurable application port, defaults to 8081
-        var proxyPort = process.env.PORT || 8081;
-        var httpServer = http.createServer(app);
-        httpServer.listen(proxyPort, function() {
-            console.log('Listening to port ' + proxyPort + '...');
+/**
+ * GET /routes - запрос всех маршрутов из input.txt
+ * @return {[RouteInfo]}
+ */
+app.get("^/routes$", function (req, res) {
+    res.setHeader("Content-Type", "application/json");
+    res.writeHead(200);
+    calculateAllRoutes().then(function (routes) {
+        res.write(JSON.stringify(routes));
+        res.end();
+    });
+});
+
+/**
+ * Функции по работе с Yandex API
+ */
+
+/**
+ * Получение маршрута по запрошенным данным
+ */
+function calculateRoute(data) {
+    var promise = new Promise(function (resolve) {
+        requestRoute(data, function (text) {
+            resolve({
+                data: data,
+                route: text
+            });
         });
     });
-};
+    return promise;
+}
 
-function executeRequest(res) {
-    var lines = fs.readFileSync("input.txt", {encoding: "utf-8"}).split("\n");
+/**
+ * Получение всех маршрутов по данным из input.txt
+ */
+function calculateAllRoutes() {
+    var promise = new Promise(function (resolve) {
+        var lines = fs.readFileSync("input.txt", {encoding: "utf-8"}).split("\n");
 
-    var counter;
+        var routes = new Array(lines.length);
 
-    function appendRoute(index, text) {
-        fs.appendFile("result.txt", "line " + index + ": " + text + "\n");
-        counter++;
-        if (counter == lines.length) {
-            fs.appendFile("result.txt", "\n-----------------------------------\n");
-            if (res) {
-                res.end();
+        var counter;
+
+        function appendRoute(index, text) {
+            routes[index] = {
+                data: lines[index],
+                route: text
+            };
+            counter++;
+            if (counter == lines.length) {
+                resolve(routes);
             }
         }
-    }
 
-    counter = 0;
+        counter = 0;
 
-    fs.appendFile("result.txt", new Date().toLocaleString() + "\n\n");
-    lines.forEach(function (line, index) {
-        getRoute(line).then(function(route) {
-            appendRoute(index, route.getHumanJamsTime().split("&")[0]);
-        }, function (error) {
-            appendRoute(index, "Error: " + error.message);
+        lines.forEach(function (line, index) {
+            requestRoute(line, function (text) {
+                appendRoute(index, text);
+            });
         });
+    });
+    return promise;
+}
+
+function requestRoute(line, handler) {
+    return getRoute(line).then(function(route) {
+        return handler(route.getHumanJamsTime().split("&")[0]);
+    }, function (error) {
+        return handler("Error: " + error.message);
     });
 }
 
@@ -97,4 +143,37 @@ function getRoute(line) {
         ])
     }
 }
+
+/**
+ * Создание виртуального окна для yandex-карт, потому что yandex-карты не поддерживают работу в окружении NodeJS
+ */
+
+//виртуальный html
+var html = '<html><head><script src="http://api-maps.yandex.ru/2.0/?load=package.standard,package.geoObjects,package.route&lang=ru-RU" type="text/javascript"></script></head><body></body></html>';
+//виртуальный document
+var doc = jsdom.jsdom(html, {
+    features: {
+        FetchExternalResources   : ['script'],
+        ProcessExternalResources : ['script'],
+        MutationEvents           : '2.0'
+    }
+});
+//виртуальный window
+var window = doc.defaultView;
+window.onload = function() {
+    window.ymaps.ready(function() {
+        // configurable application port, defaults to 8081
+        var proxyPort = process.env.PORT || 8081;
+        var httpServer = http.createServer(app);
+
+        //nodejs сервер стартует по готовности виртульного окна и yandex-карт
+        httpServer.listen(proxyPort, function() {
+            console.log('Listening to port ' + proxyPort + '...');
+        });
+    });
+};
+
+/**
+ * -------------------------------------------------
+ */
 
